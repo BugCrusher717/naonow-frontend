462a0903603cf19c9fe2e288eab11f8cf9275dde
Abakour <abakour.dev@gmail.com>
2022-07-08 03:26:29 -0700

    first commit

---
+++ src/pages/FAQ.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+import '../assets/styles/dashboard.scss';
+
+const FAQ = () => {
+  return (
+    <Layout>
+      FAQ
+    </Layout>
+  )
+}
+
+export default FAQ;
